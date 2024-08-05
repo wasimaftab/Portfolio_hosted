0ba11123b7187bc51c376c75f60813e8ce051e21
wasimaftab <wasimgradapplication@gmail.com>
2024-08-06 01:07:48 +0200

    portfolio updated

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path'; // Add this import
 
 export default defineConfig({
   root: 'src',
@@ -10,6 +11,14 @@ export default defineConfig({
     outDir: '../dist',
     emptyOutDir: true, // Clear the output directory before each build
     sourcemap: false, // Disable source maps
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        resume: resolve(__dirname, 'src/resume.html'),
+        projects: resolve(__dirname, 'src/projects.html'),
+        contact: resolve(__dirname, 'src/contact.html'),
+      }
+    }
   },
   publicDir: '../public',
 });
