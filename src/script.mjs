6c7e82f836c217e072ed0956d9404227736bb9ae
wasimaftab <wasimgradapplication@gmail.com>
2024-08-06 00:51:49 +0200

    portfolio updated

--- src/script.mjs
+++ src/script.mjs
@@ -1,4 +1,18 @@
+"use strict";
+
+import Swal from 'sweetalert2'
+
 // Add any interactive JS here, if necessary
 document.addEventListener('DOMContentLoaded', function () {
     console.log('Portfolio loaded');
+    document.getElementById("submit-contact").addEventListener("click", async (event) => {
+        Swal.fire({
+            icon: 'success',
+            title: "Work in progress",
+            // text: result.output,
+            allowOutsideClick: false
+        });
+        event.preventDefault(); // Prevent form submission
+        console.log("Work in progress");
+    });
 });
