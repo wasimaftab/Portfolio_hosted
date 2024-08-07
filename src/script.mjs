b322955888628d6dc68d9fe1af48d442383a23a1
Wasim Aftab <wasimgradapplication@gmail.com>
2024-08-07 14:19:34 +0200

    Portfolio code updated

--- src/script.mjs
+++ src/script.mjs
@@ -5,14 +5,36 @@ import Swal from 'sweetalert2'
 // Add any interactive JS here, if necessary
 document.addEventListener('DOMContentLoaded', function () {
     console.log('Portfolio loaded');
-    document.getElementById("submit-contact").addEventListener("click", async (event) => {
-        Swal.fire({
-            icon: 'success',
-            title: "Work in progress",
-            // text: result.output,
-            allowOutsideClick: false
+    const submitButton = document.getElementById("submit-contact");
+    if (submitButton) {
+        submitButton.addEventListener("click", async (event) => {
+            Swal.fire({
+                icon: 'success',
+                title: "Work in progress",
+                allowOutsideClick: false
+            });
+            event.preventDefault(); // Prevent form submission
+            console.log("Work in progress");
         });
-        event.preventDefault(); // Prevent form submission
-        console.log("Work in progress");
-    });
+    }
+
+    // const toggleButton = document.querySelector('.navbar-toggle');
+    // if (toggleButton){
+    //     const navbarCollapse = document.querySelector('.navbar-collapse');
+    //     toggleButton.addEventListener('click', function () {
+    //         navbarCollapse.classList.toggle('open');
+    //     });
+    // }
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleButton = document.querySelector('.navbar-toggle');
+    const navbarCollapse = document.querySelector('.navbar-collapse');
+
+    if (toggleButton && navbarCollapse) {
+        toggleButton.addEventListener('click', function () {
+            navbarCollapse.classList.toggle('open');
+        });
+    }
+});
+
